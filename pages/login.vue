6487dfe08568e759d4d8f6d714331476cf0c8326
Wilfred.ADJOVI <Wilfrieda216@gmail.com>
2024-05-25 11:58:54 +0200

    mise à jour

---
+++ pages/login.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="prose">
+    <h1>Connexion</h1>
+    <form action="">
+      <label for="">
+        Email
+      </label>
+      <input type="text">
+      <label for="">Mot de passe</label>
+      <input type="password">
+      <button type="button">Se connecter</button>
+    </form>
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style></style>
---
+++ pages/logout.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    Log out 
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+
+</style>
