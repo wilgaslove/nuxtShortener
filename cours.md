0aaceffb14c0f5c4465a15958f51c5529e7c1694
Wilfred.ADJOVI <Wilfrieda216@gmail.com>
2024-05-30 18:05:03 +0200

    mise ajout du formulaire et installation d\axios

---
+++ cours.md
@@ -0,0 +1,2 @@
+## Plugin Nuxt
+En Nuxt, les plugins sont utilisés pour injecter des fonctionalités glabales ou pour configurer des bibliothèques tierces qui doivent être initialisées avant que l'application ne démarre.
--- pages/register.vue
+++ pages/register.vue
@@ -1,27 +1,27 @@
 <template>
   <div class="prose">
-    <h1>Connexion</h1>
-    <form action="">
+    <h1>Inscription</h1>
+    <form @submit.prevent="register(form)">
       <label>
         Nom et prénom
-        <input type="text">
+        <input type="text" v-model="form.name">
       </label>
       <label>
         Email
-        <input type="email">
+        <input type="email" v-model="form.email">
       </label>
       <label>
         Mot de passe
-        <input type="password">
+        <input type="password" v-model="form.password">
       </label>
       <label>
        Confirmation Mot de passe
-        <input type="password">
+        <input type="password" v-model="form.password_confirmation">
       </label>
-      <button type="button" class="btn">S'inscrire'</button>
+      <button type="submit" class="btn">S'inscrire'</button>
     </form>
     <p>
-      Pas de compte ?
+      Vous avez déjjà un compte ?
       <NuxtLink class="uderline text-lime-600" to="/login">connectez-vous!</NuxtLink>
     </p>
   </div>
@@ -31,6 +31,25 @@
   definePageMeta({
     layout: "centered"
   });
+
+  interface RegisterPayload {
+    name: string;
+    email: string;
+    password: string;
+    password_confirmation: string;
+  };
+
+  const form = ref<RegisterPayload>({
+    name: "",
+    email: "",
+    password: "",
+    password_confirmation: "",
+  });
+
+  async function register(payload: RegisterPayload) {
+    alert('hello')
+    console.log({payload});
+  }
 </script>
 
 <style></style>
