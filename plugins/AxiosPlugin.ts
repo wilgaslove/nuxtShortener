8f8cd15bde21801f0b2465bd6c0f36244e97e4bf
Wilfred.ADJOVI <Wilfrieda216@gmail.com>
2024-05-30 19:05:48 +0200

    cours plugin axios

--- plugins/AxiosPlugin.ts
+++ plugins/AxiosPlugin.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export default defineNuxtPlugin(async (nuxtApp) => {
-    //configuration de base pour que les requettes requêtes soient accepter par le backent
-    axios.defaults.baseURL = 'http://localhost:8000';
+    //configuration de base pour que les requettes requêtes soient accepter par le backend
+    axios.defaults.baseURL = useRuntimeConfig().public.apiUrl;
     axios.defaults.headers.common['Content-type'] = "application/json"; // on envoie du json au serve
     axios.defaults.headers.common['Accept'] = "application/json"; // on accepte du json du serve
     axios.defaults.withCredentials = true;
